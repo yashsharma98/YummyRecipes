98d01f7c5caed34ec05be4b82b34e4366c83c8e6
Yash Sharma <yasharma2324@gmail.com>
2024-12-14 13:43:24 +0530

    2nd commit

--- test_project/settings.py
+++ test_project/settings.py
@@ -285,6 +285,8 @@ STORAGES = {
 }
 
 
+# Configured for AWS S3
+
 if 'DATABASE_URL' in os.environ:
 
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -299,6 +301,20 @@ if 'DATABASE_URL' in os.environ:
 
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/images/'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# else:
+#     AWS_ACCESS_KEY_ID = ''  # Enter your AWS_ACCESS_KEY_ID
+#     AWS_SECRET_ACCESS_KEY = ''    # Enter your AWS_SECRET_ACCESS_KEY
+#     AWS_STORAGE_BUCKET_NAME = ''     # Enter your AWS_STORAGE_BUCKET_NAME
+#     AWS_S3_REGION_NAME = ''  # Enter your AWS_S3_REGION_NAME
+
+#     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+#     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+#     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+#     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/images/'
+#     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 TINYMCE_DEFAULT_CONFIG = {
